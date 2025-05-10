66fcc02bb56d48accdada54a5b96e3175e0428f5
Marco Castelan <marcocastelan@gmail.com>
2025-05-09 18:16:57 -0600

    Ecommerce App
    Inital Commit

---
+++ Services/Catalog/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities;
+
+public class Product:BaseEntity
+{
+    [BsonElement("Name")]
+    public string Name { get; set; }
+
+    public string Summary { get; set; }
+
+    public string Description { get; set; }
+
+    public string ImageFile { get; set; }
+    public ProductBrand Brand { get; set; }
+    public ProductType Type { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+    public decimal Price { get; set; }
+
+}
---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+
+    Task<Product> GetProductById(string id);
+
+    Task<IEnumerable<Product>> GetProductsByName(string name);
+    Task<IEnumerable<Product>> GetProductsByBrand(string name);
+
+    Task<Product> CreateProduct(Product product);
+
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+
+   
+}
